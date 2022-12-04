514ae5e237aceb3722d0ff85a8253a5ed0f0f69c
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-12-04 13:09:04 -0500

    stuff

--- bingus_settings/src/setting/bingus_setting.rs
+++ bingus_settings/src/setting/bingus_setting.rs
@@ -14,4 +14,16 @@ mod settings {
     #[derive(BingusSettingTrait)]
     #[bingus_setting(setting_type = "bool")]
     pub struct BoolSetting(bool);
+
+    #[derive(BingusSettingTrait)]
+    #[bingus_setting(setting_type = "u32")]
+    pub struct IntSetting(u32);
+
+    #[derive(BingusSettingTrait)]
+    #[bingus_setting(setting_type = "f32")]
+    pub struct FloatSetting(f32);
+
+    #[derive(BingusSettingTrait)]
+    #[bingus_setting(setting_type = "[f32; 2]")]
+    pub struct RangeSetting([f32; 2]);
 }
