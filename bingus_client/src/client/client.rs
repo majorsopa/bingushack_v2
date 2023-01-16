8099b9a0669ca3fcccf6d79fb1903b0444011554
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-01-16 17:05:49 -0500

    so close

--- bingus_client/src/client/client.rs
+++ bingus_client/src/client/client.rs
@@ -1,4 +1,6 @@
 use bingus_module::prelude::{BingusModule, populate_modules};
+use eframe::egui;
+use bingus_ui::module_widget;
 
 pub struct BingusClient {
     modules: Vec<BingusModule>,
@@ -12,4 +14,24 @@ impl BingusClient {
 
         new_self
     }
+}
+
+impl eframe::App for BingusClient {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("bingushack");
+            ui.separator();
+            ui.label("Modules:");
+            ui.separator();
+            for module in &mut self.modules {
+                ui.add(module_widget(module));
+            }
+        });
+    }
+}
+
+pub fn run_client() {
+    let app = BingusClient::new();
+    let options = eframe::NativeOptions::default();
+    eframe::run_native("bingushack", options, Box::new(|_cc| Box::new(app)));
 }
