3249fc98539de1726574a63359e011368f865f39
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-01-19 16:41:06 -0500

    forgor

--- bingushack/src/lib.rs
+++ bingushack/src/lib.rs
@@ -150,7 +150,9 @@ fn swapbuffers_hook(hdc: HDC) -> winapi::ctypes::c_int {
             wglMakeCurrent(hdc, *local_new_context.get_mut());
 
             for module in modules.lock().unwrap().iter_mut() {
-                module.render();
+                if module.get_enabled().0.get_bool() {
+                    module.render();
+                }
             }
 
             let local_old_context = OLD_CONTEXT.get_mut().unwrap();
