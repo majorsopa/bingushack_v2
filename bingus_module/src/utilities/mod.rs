0fd101aebb09315f5b94d2daf73145b0cc325004
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-01-20 20:05:12 -0500

    add to mod

--- bingus_module/src/utilities/mod.rs
+++ bingus_module/src/utilities/mod.rs
@@ -3,9 +3,11 @@ mod player_util;
 mod client_util;
 mod item_util;
 mod inventory_util;
+mod entity_util;
 
 pub use chat_util::*;
 pub use player_util::*;
 pub use client_util::*;
 pub use item_util::*;
 pub use inventory_util::*;
+pub use entity_util::*;
