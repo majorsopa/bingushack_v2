057dacfa274c072570cd26e75a8edf9939b99a37
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-01-22 17:39:45 -0500

    yuh

--- bingus_ui/src/widgets/keybind_widget.rs
+++ bingus_ui/src/widgets/keybind_widget.rs
@@ -1,9 +1,15 @@
-use eframe::egui::{self, TextEdit, TextStyle};
+use eframe::{egui::{self, TextEdit, TextStyle}, epaint::Color32};
 
 fn keybind_ui(ui: &mut egui::Ui, key: &mut String) -> egui::Response {
-    ui.add(
-        TextEdit::singleline(key).font(TextStyle::Monospace)
-    )
+    let castable = key.parse::<u32>().is_ok();
+    let text_widget = TextEdit::singleline(key).font(TextStyle::Monospace);
+    // if castable to u32
+    let text_widget = if !castable {
+        text_widget.text_color(Color32::RED)
+    } else {
+        text_widget
+    };
+    ui.add(text_widget)
 }
 
 pub fn keybind_widget(key: &mut String) -> impl egui::Widget + '_ {
