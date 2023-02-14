2400db8b1dd06f33bb7937161d44d95699dfe8ce
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-02-14 10:42:46 -0500

    start aimbot

---
+++ bingus_module/src/module/modules/aimbot.rs
@@ -0,0 +1,32 @@
+use crate::crate_prelude::*;
+
+fn tick(aimbot: &mut Aimbot, env: JNIEnv, mappings_manager: &MappingsManager) {
+
+}
+
+#[derive(BingusModuleTrait)]
+#[add_bingus_fields]
+#[bingus_module(name = "Aimbot", tick_method = "tick(self, _env, _mappings_manager)", settings_list_fields = "[range, horizontal_radius, vertical_radius, player_only]")]
+pub struct Aimbot {
+    range: (BingusSetting, &'static str, Option<[f32; 2]>),
+    horizontal_radius: (BingusSetting, &'static str, Option<[f32; 2]>),
+    vertical_radius: (BingusSetting, &'static str, Option<[f32; 2]>),
+    player_only: (BingusSetting, &'static str, Option<[f32; 2]>),
+}
+
+impl MakeNewBingusModule for Aimbot {
+    fn new() -> Self {
+        Self {
+            range: (BingusSetting::FloatSetting(3.5.into()), "range", Some([0.1, 6.0])),
+            horizontal_radius: (BingusSetting::FloatSetting(20.0.into()), "horizontal radius", Some([1.0, 90.0])),
+            vertical_radius: (BingusSetting::FloatSetting(15.0.into()), "vertical radius", Some([1.0, 90.0])),
+            player_only: (BingusSetting::BoolSetting(true.into()), "player only", None),
+
+            __enabled_bool_setting: (BingusSetting::BoolSetting(false.into()), "enabled", None),
+            __keybind_setting: (BingusSetting::KeySetting(String::from("").into()), "keybind", None),
+            __env: None,
+            __mappings_manager: None,
+            __prev_enabled: false,
+        }
+    }
+}
--- bingus_module/src/module/modules/mod.rs
+++ bingus_module/src/module/modules/mod.rs
@@ -2,8 +2,10 @@ mod autototem;
 mod triggerbot;
 mod totem_assist;
 mod esp;
+mod aimbot;
 
 pub use self::autototem::*;
 pub use self::triggerbot::*;
 pub use self::totem_assist::*;
 pub use self::esp::*;
+pub use self::aimbot::*;
