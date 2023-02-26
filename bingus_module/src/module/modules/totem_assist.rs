c6915391181f9a3b6bf23ae90813f22c30224454
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-02-26 16:47:23 -0500

    add delay to assist

--- bingus_module/src/module/modules/totem_assist.rs
+++ bingus_module/src/module/modules/totem_assist.rs
@@ -1,7 +1,41 @@
+use std::time::SystemTime;
+
+use rand::Rng;
+
 use crate::crate_prelude::*;
 
 
 fn tick(totem_assist: &mut TotemAssist, env: JNIEnv, mappings_manager: &MappingsManager) {
+    if totem_assist.time_since_lost_totem.is_none() {
+        totem_assist.time_since_lost_totem = Some(SystemTime::now());
+    }
+    match totem_assist.randomly_chosen_time {
+        Some(time) => {
+            let time_since_lost_totem = totem_assist.time_since_lost_totem.unwrap();
+            let time_since_lost_totem = time_since_lost_totem.elapsed().unwrap().as_millis();
+            if time_since_lost_totem >= time {
+                totem_assist.time_since_lost_totem = None;
+                totem_assist.randomly_chosen_time = None;
+            } else {
+                return;
+            }
+        }
+        None => {
+            let new_random_delay: u128 = {
+                let bounds = totem_assist.delay_setting.0.get_range();
+                if bounds[0] == bounds[1] {
+                    bounds[0] as u128
+                } else {
+                    let mut rng = rand::thread_rng();
+                    rng.gen_range((bounds[0] as u128)..(bounds[1] as u128))
+                }
+            };
+            totem_assist.randomly_chosen_time = Some(new_random_delay);
+            return;
+        }
+    }
+
+
     let minecraft_client = get_minecraft_client(env, mappings_manager);
     let player = match get_player_checked(env, mappings_manager, minecraft_client) {
         Some(player) => player,
@@ -71,15 +105,21 @@ fn tick(totem_assist: &mut TotemAssist, env: JNIEnv, mappings_manager: &Mappings
 
 #[derive(BingusModuleTrait)]
 #[add_bingus_fields]
-#[bingus_module(name = "TotemAssist", tick_method = "tick(self, _env, _mappings_manager)", settings_list_fields = "[fill_hotbar]")]
+#[bingus_module(name = "TotemAssist", tick_method = "tick(self, _env, _mappings_manager)", settings_list_fields = "[delay_setting, fill_hotbar]")]
 pub struct TotemAssist {
+    delay_setting: (BingusSetting, &'static str, Option<[f32; 2]>),
     fill_hotbar: (BingusSetting, &'static str, Option<[f32; 2]>),
+    randomly_chosen_time: Option<u128>,
+    time_since_lost_totem: Option<SystemTime>,
 }
 
 impl MakeNewBingusModule for TotemAssist {
     fn new() -> Self {
         Self {
+            delay_setting: (BingusSetting::RangeSetting([30.0, 45.0].into()), "delay (ms)", Some([1.0, 120.0])),
             fill_hotbar: (BingusSetting::BoolSetting(true.into()), "fill hotbar", None),
+            randomly_chosen_time: None,
+            time_since_lost_totem: None,
             __enabled_bool_setting: (BingusSetting::BoolSetting(false.into()), "enabled", None),
             __keybind_setting: (BingusSetting::KeySetting(String::from("").into()), "keybind", None),
             __env: None,
